c473c8ab930dea5561152483d9eb86b9e9bceabb
larifreitas <b.larissafs@gmail.com>
2025-02-06 19:20:59 -0300

    adicionado leitura depontos e detecção na visualização

--- main.py
+++ main.py
@@ -2,11 +2,6 @@ import cv2
 import numpy as np
 import mediapipe as mp
 
-# device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-# print(f"Usando : {device}")
-# model = YOLO('best.pt')
-# model.to(device)
-
 cap = cv2.VideoCapture('mulher_piscando.mp4')
 
 mediapipe_solutions = mp.solutions.face_mesh
@@ -15,19 +10,21 @@ face_mesh = mediapipe_solutions.FaceMesh(refine_landmarks=True)
 left_eye = [362,385,387,263,373,380]
 right_eye = [33,160,158,133,153,144]
 
-
 while cap.isOpened():
     ret, frame = cap.read()
     if not ret:
         break
 
-    resized_frame = cv2.resize(frame, (640, 416)) # 640, 480
-    face_frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    results = face_mesh.process(face_frame_rgb)
+    frame = cv2.resize(frame, (640, 416)) # 640, 480
+    #frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    results = face_mesh.process(frame)
 
     if results.multi_face_landmarks:
         for face_landmarks in results.multi_face_landmarks:
             height, width, _ = frame.shape
+
+            x1, y1 = 0, 0
+            x2, y2 = width, height
             landmarks = []
 
             for l in face_landmarks.landmark:
@@ -35,7 +32,19 @@ while cap.isOpened():
                 y = int(l.y * height)
                 landmarks.append((x, y))
 
-    cv2.imshow('output', resized_frame)
+            Xmin = min([landmarks[i][0] for i in range(len(landmarks))])
+            Ymin = min([landmarks[i][1] for i in range(len(landmarks))])
+            Xmax = max([landmarks[i][0] for i in range(len(landmarks))])
+            Ymax = max([landmarks[i][1] for i in range(len(landmarks))])
+
+            # face
+            cv2.rectangle(frame, (Xmin, Ymin),(Xmax, Ymax), (100, 20, 200), 2, 2)
+
+            # olhos
+            for i in left_eye + right_eye:
+                cv2.circle(frame[y1:y2, x1:x2], landmarks[i], 2, (0, 255, 0), -1)
+
+    cv2.imshow('Detecetor de sonolencia', frame)
     if cv2.waitKey(1) & 0xFF == ord('q') == ord(27):
         break
 

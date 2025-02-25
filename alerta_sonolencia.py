8c78d2e23620adf02b00ccad82db2ed3bcdbd7fd
larifreitas <b.larissafs@gmail.com>
2025-02-25 07:58:41 -0300

    removido pontos chave dos olhos

--- alerta_sonolencia.py
+++ alerta_sonolencia.py
@@ -2,20 +2,16 @@ import cv2
 import numpy as np
 import time
 
-# left_eye = [362,385,387,263,373,380]
-# right_eye = [33,160,158,133,153,144]
 
 START_THRESH = 0.50
-CLOSED_TIME_THRESH = 5 # limiar para limite de olhos fechados
-first_time_closed = None # tempo em que os olhos começam a se fechar
+CLOSED_TIME_THRESH = 5
+first_time_closed = None
 sonolencia_triggered = False  # flag para saber se o alarme já foi disparado
 
 def calculo_ear_sonolencia(landmarks, eye_idx):
     p1, p2, p3, p4, p5, p6 = [landmarks[i] for i in eye_idx]
-    #horizontal
     A=np.linalg.norm(np.array(p2) - np.array(p6))
     B=np.linalg.norm(np.array(p3) - np.array(p5))
-    #vertical
     C=np.linalg.norm(np.array(p1) - np.array(p2)) # ponto 4- bug
 
     ear = (A+B)/(2.0*C)
@@ -29,7 +25,6 @@ def verificar_sonolencia(frame, landmarks, left_eye, right_eye, arduino):
     avg = (left_ear+right_ear)/2.0
     print(f"Valor de EAR: {avg:.2f}")
 
-    # verificação pros olhos fechados
     if avg < START_THRESH:
         if first_time_closed is None: # se for 1ª vez que olhos fecham
             first_time_closed = time.time()

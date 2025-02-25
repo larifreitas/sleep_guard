99a6fc18e74f70703c513a234ff6bfa2ce45cb1c
larifreitas <b.larissafs@gmail.com>
2025-02-25 07:57:19 -0300

    adicionado trigger de sonolencia para arduino

--- alerta_sonolencia.py
+++ alerta_sonolencia.py
@@ -8,6 +8,7 @@ import time
 START_THRESH = 0.50
 CLOSED_TIME_THRESH = 5 # limiar para limite de olhos fechados
 first_time_closed = None # tempo em que os olhos começam a se fechar
+sonolencia_triggered = False  # flag para saber se o alarme já foi disparado
 
 def calculo_ear_sonolencia(landmarks, eye_idx):
     p1, p2, p3, p4, p5, p6 = [landmarks[i] for i in eye_idx]
@@ -20,8 +21,8 @@ def calculo_ear_sonolencia(landmarks, eye_idx):
     ear = (A+B)/(2.0*C)
     return ear
 
-def verificar_sonolencia(frame, landmarks, left_eye, right_eye):
-    global first_time_closed
+def verificar_sonolencia(frame, landmarks, left_eye, right_eye, arduino):
+    global first_time_closed, sonolencia_triggered
 
     left_ear = calculo_ear_sonolencia(landmarks,left_eye)
     right_ear = calculo_ear_sonolencia(landmarks,right_eye)
@@ -32,9 +33,13 @@ def verificar_sonolencia(frame, landmarks, left_eye, right_eye):
     if avg < START_THRESH:
         if first_time_closed is None: # se for 1ª vez que olhos fecham
             first_time_closed = time.time()
+            sonolencia_triggered = False
         else:
             if time.time() - first_time_closed >= CLOSED_TIME_THRESH:
                 cv2.putText(frame, "Ocorrencia de sonolencia!", (30, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255),4)
+                arduino.write(b"SONOLENCIA\n")
+                sonolencia_triggered = True
             
     else:
         first_time_closed = None
+        sonolencia_triggered = False

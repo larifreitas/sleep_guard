6a007623d5bc2ae21fac34cd3c5145eb22882666
larifreitas <b.larissafs@gmail.com>
2025-02-25 18:53:04 -0300

    adicionado serviço de alarme para os alertas

--- Arduino/sleep_guard_alarm/sleep_guard_alarm.ino
+++ Arduino/sleep_guard_alarm/sleep_guard_alarm.ino
@@ -2,31 +2,27 @@ void setup() {
   Serial.begin(9600);
   pinMode(10, OUTPUT);
   digitalWrite(10, HIGH); // Garante que o buzzer fique desligado inicialmente
-  Serial.println("Setup completo");
 }
 
 void loop() {
   if (Serial.available() > 0) {
-    String comando = Serial.readStringUntil('\n');
-    comando.trim();
-    Serial.print("Comando recebido: ");
-    Serial.println(comando);
-    
-    if (comando == "SONOLENCIA") {
-      for (int i = 0; i < 3; i++) {
-        digitalWrite(10,LOW);
-        delay(200);
-        digitalWrite(10, HIGH);
-        delay(100);
-      }
-      digitalWrite(10,HIGH);
-    } else if (comando == "FADIGA") {
-      for (int i; i<=1;i++){
-        digitalWrite(10,LOW);
-        delay(200);
-        digitalWrite(10, HIGH);
-        delay(100);
+  String comando = Serial.readStringUntil('\n');
+  comando.trim();
+  Serial.print("Comando recebido: ");
+  Serial.println(comando);
+  
+  if (comando == "SONOLENCIA") {
+    for (int i = 0; i < 3; i++) {
+      digitalWrite(10, LOW);  // Aciona o buzzer
+      delay(200);
+      digitalWrite(10, HIGH); // Desliga o buzzer
+      delay(100);
     }
-    digitalWrite(10,HIGH);
+  } else if (comando == "FADIGA") {
+    digitalWrite(10, LOW);
+    delay(200);
+    digitalWrite(10, HIGH);
+    delay(100);
+    }
   }
 }

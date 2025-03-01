85f12304850106e0636a074179d9bc807faba503
larifreitas <b.larissafs@gmail.com>
2025-03-01 20:16:52 -0300

    add alerta de fadiga 2 beeps

--- Arduino/sleep_guard_alarm/sleep_guard_alarm.ino
+++ Arduino/sleep_guard_alarm/sleep_guard_alarm.ino
@@ -6,23 +6,34 @@ void setup() {
 
 void loop() {
   if (Serial.available() > 0) {
-  String comando = Serial.readStringUntil('\n');
-  comando.trim();
-  Serial.print("Comando recebido: ");
-  Serial.println(comando);
+    String comando = Serial.readStringUntil('\n');
+    comando.trim();
+    Serial.print("Comando recebido: ");
+    Serial.println(comando);
   
-  if (comando == "SONOLENCIA") {
-    for (int i = 0; i < 3; i++) {
-      digitalWrite(10, LOW);  // Aciona o buzzer
-      delay(200);
-      digitalWrite(10, HIGH); // Desliga o buzzer
-      delay(100);
-    }
-  } else if (comando == "FADIGA") {
-    digitalWrite(10, LOW);
-    delay(200);
-    digitalWrite(10, HIGH);
-    delay(100);
+    if (comando == "SONOLENCIA") {
+      for (int i = 0; i <= 3; i++) {
+        digitalWrite(10, LOW);  // Aciona o buzzer
+        delay(200);
+        digitalWrite(10, HIGH); // Desliga o buzzer
+        delay(100);
+      }
+    } 
+    else if (comando == "FADIGA") {
+      for (int i = 0; i <= 1; i++) {      
+        digitalWrite(10, LOW);  // Aciona o buzzer
+        delay(200);
+        digitalWrite(10, HIGH); // Desliga o buzzer
+        delay(100);
+      }
+    } 
+    else if (comando == "FADIGA_2") {
+      for (int i = 0; i <= 2; i++) {      
+        digitalWrite(10, LOW);  // Aciona o buzzer
+        delay(200);
+        digitalWrite(10, HIGH); // Desliga o buzzer
+        delay(100);
+      }
     }
   }
 }
